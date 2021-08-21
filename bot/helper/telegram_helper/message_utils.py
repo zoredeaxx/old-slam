53472830c065db5dd87f371c62d1af789c9caa8d
zoredeaxx <87483466+zoredeaxx@users.noreply.github.com>
2021-08-21 19:01:36 +0530

    Update message_utils.py

--- bot/helper/telegram_helper/message_utils.py
+++ bot/helper/telegram_helper/message_utils.py
@@ -67,20 +67,22 @@ def delete_all_messages():
 
 
 def update_all_messages():
+    total, used, free = shutil.disk_usage('.')
+    free = get_readable_file_size(free)
+    currentTime = get_readable_time(time.time() - botStartTime)
     msg = get_readable_message()
-    msg += f"<b>BOT UPTIME 🟢 :</b> <b>{get_readable_time((time.time() - botStartTime))}</b>\n\n" \
-           f"<b>CPU : {psutil.cpu_percent()}%</b>" \
-           f" <b>RAM : {psutil.virtual_memory().percent}%</b>" \
-           f" <b>DISK : {psutil.disk_usage('/').percent}%</b>"
+    msg += f"<b>CPU:</b> {psutil.cpu_percent()}%" \
+           f" <b>RAM:</b> {psutil.virtual_memory().percent}%" \
+           f" <b>DISK:</b> {psutil.disk_usage('/').percent}%"
     with download_dict_lock:
         dlspeed_bytes = 0
         uldl_bytes = 0
         for download in list(download_dict.values()):
             speedy = download.speed()
             if download.status() == MirrorStatus.STATUS_DOWNLOADING:
-                if 'KiB/s' in speedy:
+                if 'K' in speedy:
                     dlspeed_bytes += float(speedy.split('K')[0]) * 1024
-                elif 'MiB/s' in speedy:
+                elif 'M' in speedy:
                     dlspeed_bytes += float(speedy.split('M')[0]) * 1048576 
             if download.status() == MirrorStatus.STATUS_UPLOADING:
                 if 'KB/s' in speedy:
@@ -89,7 +91,7 @@ def update_all_messages():
                     uldl_bytes += float(speedy.split('M')[0]) * 1048576
         dlspeed = get_readable_file_size(dlspeed_bytes)
         ulspeed = get_readable_file_size(uldl_bytes)
-        msg += f"\n<b>DOWN :</b> <b>{dlspeed}ps</b> 🌝 <b>||</b> <b>UP :</b> <b>{ulspeed}ps</b> 🌚\n"
+        msg += f"\n<b>FREE:</b> {free} | <b>BOT UPTIME:</b> {currentTime}\n<b>DL:</b> {dlspeed}ps 🔻 | <b>UL:</b> {ulspeed}ps 🔺\n"
     with status_reply_dict_lock:
         for chat_id in list(status_reply_dict.keys()):
             if status_reply_dict[chat_id] and msg != status_reply_dict[chat_id].text:
@@ -102,22 +104,23 @@ def update_all_messages():
                 status_reply_dict[chat_id].text = msg
 
 
-
 def sendStatusMessage(msg, bot):
+    total, used, free = shutil.disk_usage('.')
+    free = get_readable_file_size(free)
+    currentTime = get_readable_time(time.time() - botStartTime)
     progress = get_readable_message()
-    progress += f"<b>BOT UPTIME 🟢 :</b> <b>{get_readable_time((time.time() - botStartTime))}</b>\n\n" \
-           f"<b>CPU : {psutil.cpu_percent()}%</b>" \
-           f" <b>RAM : {psutil.virtual_memory().percent}%</b>" \
-           f" <b>DISK : {psutil.disk_usage('/').percent}%</b>"
+    progress += f"<b>CPU:</b> {psutil.cpu_percent()}%" \
+           f" <b>RAM:</b> {psutil.virtual_memory().percent}%" \
+           f" <b>DISK:</b> {psutil.disk_usage('/').percent}%"
     with download_dict_lock:
         dlspeed_bytes = 0
         uldl_bytes = 0
         for download in list(download_dict.values()):
             speedy = download.speed()
             if download.status() == MirrorStatus.STATUS_DOWNLOADING:
-                if 'KiB/s' in speedy:
+                if 'K' in speedy:
                     dlspeed_bytes += float(speedy.split('K')[0]) * 1024
-                elif 'MiB/s' in speedy:
+                elif 'M' in speedy:
                     dlspeed_bytes += float(speedy.split('M')[0]) * 1048576 
             if download.status() == MirrorStatus.STATUS_UPLOADING:
                 if 'KB/s' in speedy:
@@ -126,7 +129,7 @@ def sendStatusMessage(msg, bot):
                     uldl_bytes += float(speedy.split('M')[0]) * 1048576
         dlspeed = get_readable_file_size(dlspeed_bytes)
         ulspeed = get_readable_file_size(uldl_bytes)
-        progress += f"\n<b>DOWN :</b> <b>{dlspeed}ps</b> 🌝 <b>||</b> <b>UP :</b> <b>{ulspeed}ps</b> 🌚\n"
+        progress += f"\n<b>FREE:</b> {free} | <b>BOT UPTIME:</b> {currentTime}\n<b>DL:</b> {dlspeed}ps 🔻 | <b>UL:</b> {ulspeed}ps 🔺\n"
     with status_reply_dict_lock:
         if msg.message.chat.id in list(status_reply_dict.keys()):
             try:
